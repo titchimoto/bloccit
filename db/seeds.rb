882e50fc8c6e79f44a448f36da117fbebc635e83
Titch Deane <titch@hotmail.co.uk>
2018-02-01 09:54:18 -0800

    Assignment Seeding: Complete

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,6 +10,10 @@ posts = Post.all
   Comment.create!(post: posts.sample, body: RandomData.random_paragraph)
 end
 
+unique_post = Post.find_or_create_by!(title: "Unique Title", body: "Unique Body")
+
+Comment.find_or_create_by!(post: unique_post, body: "Really Unique Comment Body")
+
 puts "Seed Finished"
 puts "#{Post.count} posts created"
 puts "#{Comment.count} comments created"
