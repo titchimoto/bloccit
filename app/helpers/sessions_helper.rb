a22ab177fc8dd6622ecf77998381abc54770a7e8
Titch Deane <titch@hotmail.co.uk>
2018-02-07 13:05:30 -0800

    Assignment Sign In: Complete

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,15 +5,17 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
-
-
-
   def destroy_session(user)
     session[:user_id] = nil
   end
 
   def current_user
     User.find_by(id: session[:user_id])
+  end
+
+  def avatar_url(user)
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
   end
 
 end
