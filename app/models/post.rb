17e886c1d37eaada2e6a711c32a4f141e804b159
Titch Deane <titch@hotmail.co.uk>
2018-02-09 16:27:57 -0800

    Assignment Favorites: Complete

--- app/models/post.rb
+++ app/models/post.rb
@@ -12,6 +12,8 @@ class Post < ApplicationRecord
   validates :topic, presence: true
   validates :user, presence: true
 
+  after_create :new_post_favorite
+
   def up_votes
     votes.where(value: 1).count
   end
@@ -28,6 +30,13 @@ class Post < ApplicationRecord
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  private
+
+  def new_post_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deliver_now
   end
 
 end
